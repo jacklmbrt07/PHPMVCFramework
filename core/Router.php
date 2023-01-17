3871643f868e48f27b1cbfb4f1a9e92210313ece
jacklmbrt07 <jacklmbrt07@gmail.com>
2023-01-16 17:45:21 -0800

    first commit

---
+++ core/Router.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+    protected array $routes = [
+    ];
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        
+    }
+
+}
